85509f163e8b8e931ee97db20ba485bdab0fca0b
shamisboy <shamsay70@gmail.com>
2026-08-16 18:41:11 +0300

    last change

--- Models/database.py
+++ Models/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:shamsay321!@localhost:5432/shamis_db"
+DATABASE_URL = "postgresql+asyncpg://my_fastapi_db_36ct_user:shamsay321!@dpg-da0t81fqj5pc73b871u0-a:5432/my_fastapi_db_36ct"
 
 # Create asynchronous engine
 engine = create_async_engine(DATABASE_URL, echo=True)
