ec27e7de14b51d27c43b56c424a8d725946e781f
shamisboy <shamsay70@gmail.com>
2026-08-16 19:05:07 +0300

    last changes

--- Models/database.py
+++ Models/database.py
@@ -1,18 +1,30 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://my_fastapi_db_36ct_user:shamsay321!@dpg-da0t81fqj5pc73b871u0-a:5432/my_fastapi_db_36ct"
+DATABASE_URL = (
+    "postgresql+asyncpg://my_fastapi_db_36ct_user:"
+    "PASSWORD@dpg-da0t81fqj5pc73b871u0-a:5432/"
+    "my_fastapi_db_36ct"
+)
 
-# Create asynchronous engine
-engine = create_async_engine(DATABASE_URL, echo=True)
+# Async engine
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
 
-# Create asynchronous session
-AsyncSessionLocal = sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
+# Async session
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 # Base model
 Base = declarative_base()
 
-# Dependency function kwa FastAPI
+
+# FastAPI dependency
 async def get_db():
     async with AsyncSessionLocal() as session:
         try:
